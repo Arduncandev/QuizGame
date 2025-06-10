168b81fd8d3dde3fc2baaa5720338e87fda8fb33
Arduncandev <103537497+Arduncandev@users.noreply.github.com>
2025-06-10 13:42:36 -0500

    adds a question timer for each question

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,33 +1,46 @@
-import { useState } from "react";
-import quizCompleteImg from '../assets/quiz-complete.png'
+import { useState, useCallback } from "react";
+import quizCompleteImg from "../assets/quiz-complete.png";
+import QuestionTimer from "./QuestionTimer";
 
 import QUESTIONS from "../questions";
+import { act } from "react";
 
 export default function Quiz() {
 	const [userAnswers, setUserAnswers] = useState([]);
 
 	const activeQuestionIndex = userAnswers.length;
-    const quizIsComplete = activeQuestionIndex === QUESTIONS.length
+	const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-	function handleSelectAnswer(selectedAnswer) {
+	const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
 		setUserAnswers((prevUserAnswers) => {
 			return [...userAnswers, selectedAnswer];
 		});
+	}, []);
+
+	const handleSkipAnswer = useCallback(() => {
+		handleSelectAnswer(null);
+	}, [handleSelectAnswer]);
+
+	if (quizIsComplete) {
+		return (
+			<div id='summary'>
+				<img src={quizCompleteImg} alt='Trophy icon' />
+				<h2>Quiz Completed</h2>
+			</div>
+		);
 	}
 
-    if (quizIsComplete) {
-        return <div id="summary">
-            <img src={quizCompleteImg} alt="Trophy icon" />
-            <h2>Quiz Completed</h2>
-        </div>
-    }
+	const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+	shuffledAnswers.sort(() => Math.random() - 0.5);
 
-    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-    shuffledAnswers.sort(() => Math.random() -0.5);
-    
 	return (
 		<div id='quiz'>
 			<div id='question'>
+				<QuestionTimer
+                key={activeQuestionIndex}
+					timeout={10000}
+					onTimeout={handleSkipAnswer}
+				/>
 				<h2>{QUESTIONS[activeQuestionIndex].text}</h2>
 				<ul id='answers'>
 					{shuffledAnswers.map((answer) => (
